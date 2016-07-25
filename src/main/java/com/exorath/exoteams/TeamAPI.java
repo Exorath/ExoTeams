6eb3ca3a821950153be1212d2abff6da2bc2fb08
Toon Sevrin <twan123@live.be>
2016-07-24 19:34:10 -0700

    TeamAPI added

---
+++ src/main/java/com/exorath/exoteams/TeamAPI.java
@@ -0,0 +1,28 @@
+package com.exorath.exoteams;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by toonsev on 7/24/2016.
+ */
+public class TeamAPI {
+    private List<Team> teams = new ArrayList<>();
+
+    public static TeamAPI createAPI(){
+        return new TeamAPI();
+    }
+
+    public void addTeam(Team team){
+        teams.add(team);
+    }
+
+    public void removeTeam(Team team){
+        teams.remove(team);
+    }
+
+    public Collection getTeams() {
+        return teams;
+    }
+}
---
+++ src/test/java/com/exorath/exoteams/TeamAPITest.java
@@ -0,0 +1,81 @@
+package com.exorath.exoteams;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by toonsev on 7/24/2016.
+ */
+public class TeamAPITest {
+
+    @Test
+    public void defaultTeamSizeTest(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        assertTrue(teamAPI.getTeams().isEmpty());
+    }
+
+    @Test
+    public void addTeamContainsTest(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        Team team = new Team();
+        teamAPI.addTeam(team);
+        assertTrue(teamAPI.getTeams().contains(team));
+    }
+
+    @Test
+    public void addTeamContainsTest1(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        Team team1 = new Team();
+        Team team2 = new Team();
+        teamAPI.addTeam(team1);
+        teamAPI.addTeam(team2);
+        assertTrue(teamAPI.getTeams().contains(team1) && teamAPI.getTeams().contains(team2));
+    }
+
+    @Test
+    public void addTeamSizeTest(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        teamAPI.addTeam(new Team());
+        assertEquals(teamAPI.getTeams().size(), 1);
+    }
+
+    @Test
+    public void addTeamSizeTest1(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        teamAPI.addTeam(new Team());
+        teamAPI.addTeam(new Team());
+        assertEquals(teamAPI.getTeams().size(), 2);
+    }
+
+    @Test
+    public void removeTeamContainsTest(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        Team team = new Team();
+        teamAPI.addTeam(team);
+        teamAPI.removeTeam(team);
+        assertFalse(teamAPI.getTeams().contains(team));
+    }
+
+
+    @Test
+    public void removeTeamSizeTest(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        Team team = new Team();
+        teamAPI.addTeam(team);
+        teamAPI.removeTeam(team);
+        assertEquals(teamAPI.getTeams().size(), 0);
+    }
+
+    @Test
+    public void removeTeamSizeTest1(){
+        TeamAPI teamAPI = TeamAPI.createAPI();
+        Team team = new Team();
+        teamAPI.addTeam(team);
+        teamAPI.removeTeam(team);
+        teamAPI.addTeam(new Team());
+        assertEquals(teamAPI.getTeams().size(), 1);
+    }
+}
