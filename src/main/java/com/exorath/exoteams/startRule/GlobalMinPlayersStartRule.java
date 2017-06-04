81c199051c22d1861a227cd8645dfc0aa74f5ae6
Toon Sevrin <twan123@live.be>
2017-06-04 14:52:06 +0200

    Added global start rule

---
+++ src/main/java/com/exorath/exoteams/startRule/GlobalMinPlayersStartRule.java
@@ -0,0 +1,43 @@
+package com.exorath.exoteams.startRule;
+
+import com.exorath.exoteams.player.TeamPlayerJoinTeamEvent;
+import com.exorath.exoteams.player.TeamPlayerLeaveTeamEvent;
+
+/**
+ * Created by toonsev on 6/4/2017.
+ */
+public class GlobalMinPlayersStartRule extends GlobalStartRule {
+    private boolean canStart = false;
+    private int players;
+    private int minPlayers;
+
+    public GlobalMinPlayersStartRule(int minPlayers) {
+        this.minPlayers = minPlayers;
+    }
+
+    @Override
+    public String getReason() {
+        return "Insufficient players";
+    }
+
+    @Override
+    public void onPlayerJoin(TeamPlayerLeaveTeamEvent event) {
+        addPlayers(1);
+        evaluate();
+    }
+
+    @Override
+    public void onPlayerJoin(TeamPlayerJoinTeamEvent event) {
+        addPlayers(-1);
+        evaluate();
+    }
+
+    private synchronized void addPlayers(int amount) {
+        players = players + amount;
+    }
+
+    @Override
+    public boolean doEvaluate() {
+        return players > minPlayers;
+    }
+}
